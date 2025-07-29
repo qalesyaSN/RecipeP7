ba31e703c282a89c73664c6bac9d3fda2c816548
qalesyaSN <b3byinjector@gmail.com>
2025-07-29 19:07:00 +0700

    add filter xss

--- search.php
+++ search.php
@@ -2,20 +2,18 @@
 
 <?php
 include("inc/head.inc.php");
-// Ambil query pencarian dari AJAX request
 $searchQuery = $conn->real_escape_string($_POST["query"] ?? '');
-// Buat query SQL pencarian jika ada query
 
-    $sql = "SELECT * FROM recipe WHERE name LIKE '%$searchQuery%' ORDER BY name ASC";
+    $sql = "SELECT id, name FROM recipe WHERE name LIKE '%$searchQuery%' ORDER BY name ASC";
 
     $result = $conn->query($sql);
 
     // Tampilkan hasil pencarian
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-        echo "<div class='card mt-2 recipe-card' data-recipe-id='" . $row["id"] . "'>";
+        echo "<div class='card mt-2 recipe-card' data-recipe-id='" . htmlspecialchars($row["id"]) . "'>";
         echo "<div class='card-body'>";
-        echo "<h5 class='card-title mb-0'><i class='fa-solid fa-book-open'></i> " . $row["name"] . "</h5>";
+        echo "<h5 class='card-title mb-0'><i class='fa-solid fa-book-open'></i> " . htmlspecialchars($row["name"]) . "</h5>";
         echo "</div>";
         echo "</div>";
     }
